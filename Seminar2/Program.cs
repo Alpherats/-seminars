618f5fe3979d8c33e15d28b0de9e899dc113bf2b
Tony <Tony.gidrevich@mail.ru>
2023-04-26 11:12:25 +0300

    feat:solve seminar 2 tasks

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,20 +1,44 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
-
+Random rand = new Random();
+int number = rand.Next(100,1000);
+int sec_position = (number/10)%10;
+System.Console.WriteLine($"Цифра - {number} и вторая цифра - {sec_position}");
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+System.Console.WriteLine("Insert number");
+int num1 = Convert.ToInt32(System.Console.ReadLine());
 
-// 645 -> 5
-
-// 78 -> третьей цифры нет
-
-// 32679 -> 6
+if(num1>99)
+{
+  int find_second = num1;
+  if (num1>999)
+  {
+    find_second = num1/10;
+  }
+  find_second %=10;
+  System.Console.WriteLine($"number - {num1} third pos - {find_second}");
+}
+else
+{
+  System.Console.WriteLine("Третьей цифры нет");
+}
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-
+System.Console.WriteLine("Insert number");
+int num = Convert.ToInt32(System.Console.ReadLine());
+if (num >0 && num <7)
+{
+  if(num ==6 || num==7)
+  {
+    System.Console.WriteLine("It's weekned!");
+  }
+  else
+  {
+    System.Console.WriteLine("Time to work(");
+  }
+}
+else
+{
+  System.Console.WriteLine("Out of range");
+}
