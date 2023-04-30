6b9a5a11f4a13213b06848b9763541f9af9a8392
Tony <Tony.gidrevich@mail.ru>
2023-04-30 12:29:23 +0300

    feat:done task3 in seminar 3

---
+++ Seminar3/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿int  ReadInt(string text)
+{
+  System.Console.Write(text);
+  return Math.Abs(Convert.ToInt32(System.Console.ReadLine()));
+}
+
+void TableOfCubics(int number)
+{
+  for (int i=1; i<=number;i++)
+  {
+    System.Console.WriteLine(Math.Pow(i,3));  
+  }
+}
+
+
+int n  = ReadInt("Введите N ");
+TableOfCubics(n);
