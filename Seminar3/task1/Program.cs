416409e8f532617c95d7e4381477ec1a087c2b95
Tony <Tony.gidrevich@mail.ru>
2023-04-30 12:52:26 +0300

    feat:done task2 in seminar 3

---
+++ Seminar3/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string? str = Console.ReadLine();
+
+int num = Convert.ToInt32(Convert.ToString(str![0]));
+
+if(str.Length ==5)
+{
+  if(str[0]==str[4] && str[1]==str[3])
+  {
+    System.Console.WriteLine("Palindrome");  
+  }
+  System.Console.WriteLine(str.Length);  
+}
+else
+{
+System.Console.WriteLine("Insert number with 5 figures!");
+}
+
