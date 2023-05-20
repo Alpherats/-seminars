e18aed37848ac98d868e3d3790bdc863e168c9b4
Tony <Tony.gidrevich@mail.ru>
2023-05-20 14:00:00 +0300

    feat:fix task1 in seminar4

--- Seminar4/task1/Program.cs
+++ Seminar4/task1/Program.cs
@@ -3,7 +3,13 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+void Main()
+{
+  int a = ReadInt("Insert a: ");
+  int b = ReadInt("Insert b: ");
+  System.Console.WriteLine(CalcRate(a, b));
 
+}
 int ReadInt(string text)
 {
   System.Console.Write(text);
@@ -12,9 +18,12 @@ int ReadInt(string text)
 
 double CalcRate(int first, int second)
 {
-  return Math.Pow(first, second);
+  int bufer = first;
+  for (int i = 1; i < second; i++)
+  {
+    bufer *= first;
+  }
+  return bufer;
 }
 
-int a = ReadInt("Insert a: ");
-int b = ReadInt("Insert b: ");
-System.Console.WriteLine(CalcRate(a,b));
+Main();
