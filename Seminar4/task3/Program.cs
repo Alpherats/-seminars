bc4d0b85e778563d6144a60c93d9600003b1b780
Tony <Tony.gidrevich@mail.ru>
2023-05-20 14:11:01 +0300

    feat:done task3 in seminar 4

---
+++ Seminar4/task3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+void Main()
+{
+  int size_arr = ReadInt("Insert size of array: ");
+  int left_range = ReadInt("Insert left range of values of array: ");
+  int right_range = ReadInt("Insert right range of values of array: ");
+  PrintArray(FillArray(size_arr, left_range, right_range));
+
+}
+
+
+int ReadInt(string text)
+{
+  System.Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+  int[] arr = new int[size];
+  Random rand = new Random();
+
+  for (int i = 0; i < size; i++)
+  {
+    arr[i] = rand.Next(leftRange, rightRange);
+  }
+  return arr;
+}
+
+
+void PrintArray(int[] Array)
+{
+  Console.WriteLine("[" + string.Join(", ", Array) + "]");
+}
+
+Main();
