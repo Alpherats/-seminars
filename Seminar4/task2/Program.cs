afcb1065de63b1eda852b28702b18354c34e8ea3
Tony <Tony.gidrevich@mail.ru>
2023-05-20 14:28:43 +0300

    feat:done task2 in seminar 4

---
+++ Seminar4/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// ОФОРМИТЬ ЧЕРЕЗ VOID MAIN
+void Main()
+{
+  int num = ReadInt("Insert number: ");
+  System.Console.WriteLine(CountSum(num));
+}
+
+int ReadInt(string text)
+{
+  System.Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountSum(int num)
+{
+  int sum = 0;
+  while(num!=0)
+  {
+    sum += num%10;
+    num /=10;
+  }
+  return sum;
+}
+Main();
